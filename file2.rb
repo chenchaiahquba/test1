34a0d504c2a4a2a08965034b864d4ab70b6e8f89
chenchaiah challa <chenchaiah.challas@gmail.com>
2017-07-21 22:40:21 +0530

    changes

--- file2.rb
+++ file2.rb
@@ -1,9 +1,8 @@
-Custom Method to Create Ruby Objects
-You can pass parameters to method new and those parameters can be used to initialize class variables.
-
-When you plan to declare the new method with parameters, you need to declare the method initialize at the time of the class creation.
-
-The initialize method is a special type of method, which will be executed when the new method of the class is called with parameters.
-
-Here is the example to create initialize method −
-
+class Customer
+   @@no_of_customers = 0
+   def initialize(id, name, addr)
+      @cust_id = id
+      @cust_name = name
+      @cust_addr = addr
+   end
+end
